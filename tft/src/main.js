dce381df33f1eb990cb84d8cb4a38b7bb1df4e60
Kalota Márton <martonkalota@gmail.com>
2025-04-14 19:24:44 +0200

    Add tab switching logic and API fetch basics in JS

--- tft/src/main.js
+++ tft/src/main.js
@@ -1,24 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const API_URL = "https://retoolapi.dev/DUyWF0/data";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function switchTab(tabId) {
+  document.querySelectorAll(".tab").forEach(tab => tab.classList.remove("active"));
+  document.querySelectorAll(".tab-content").forEach(c => c.classList.remove("active"));
+  document.querySelector(`.tab[onclick*="${tabId}"]`).classList.add("active");
+  document.getElementById(tabId).classList.add("active");
 
-setupCounter(document.querySelector('#counter'))
+  if (tabId === "edit") renderAdminList("edit");
+  if (tabId === "delete") renderAdminList("delete");
+}
+async function fetchData() {
+  const res = await fetch(API_URL);
+  return await res.json();
+}
